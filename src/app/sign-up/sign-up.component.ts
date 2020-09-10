6326d2a4b9b4749fcdec10f02d53d0e54bb5cfbb
Savo Vuksan <savovuksan@gmail.com>
2020-09-10 14:34:48 +0200

    Worked on Signup

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from './../user';
 import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -9,6 +10,9 @@ import { HttpClient } from '@angular/common/http';
 export class SignUpComponent implements OnInit {
 
   user: User;
+  email: string;
+  password: string;
+  username: string;
 
   constructor(private http: HttpClient) {
     this.user = new User();
@@ -19,6 +23,9 @@ export class SignUpComponent implements OnInit {
 
   signUp(user: User){
 
+  }
+  get diagnostics(){
+    return JSON.stringify({email: this.email, password: this.password, username: this.username});
   }
 
 }
