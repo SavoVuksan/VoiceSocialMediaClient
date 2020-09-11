e4cef7e64082048f12a7fd9c1426d0301f9b4977
Savo Vuksan <savovuksan@gmail.com>
2020-09-11 21:28:02 +0200

    revert to last state

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,31 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from './../user';
-import { HttpClient } from '@angular/common/http';
-import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  user: User;
-  email: string;
-  password: string;
-  username: string;
-
-  constructor(private http: HttpClient) {
-    this.user = new User();
-   }
+  test: string;
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  signUp(user: User){
-
-  }
-  get diagnostics(){
-    return JSON.stringify({email: this.email, password: this.password, username: this.username});
   }
 
 }
